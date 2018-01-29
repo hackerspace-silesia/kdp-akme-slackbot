2a07d3b9e16f1fa7f8a8a3c6c77d7020d80f70aa
Marek Piechula <m.piechula@clearcode.cc>
2018-01-29 23:05:05 +0100

    using wit.ai

--- questions.py
+++ questions.py
@@ -1,16 +1,24 @@
 from slackbot.bot import respond_to
-from slackbot.bot import listen_to
+from pprint import pformat
+from wit import Wit
+
 import re
+import os
+
+access_token = os.environ.get('WIT_ACCESS_TOKEN')
+wit = Wit(access_token)
 
 
-@respond_to('co +to +jest (.*)', re.IGNORECASE)
+@respond_to('(.*)', re.IGNORECASE)
+#@listen_to('(.*)', re.IGNORECASE)
 def hi(message, query):
-    message.reply(query)
     message.react('+1')
-
-
-@respond_to('czym +jest +(.*)', re.IGNORECASE)
-def hi(message, query):
-    message.reply(query)
-    message.react('+1')
-
+    query = query.strip()
+    try:
+        response = wit.message(query, verbose=True)
+    except Exception as e:
+        print(e)
+        message.reply('eh :(')
+    else:
+        entities = pformat(response['entities'])
+        message.reply('```%s```' % entities)
